897010166908405ae1a44d34b55e1db9899afab5
Camila Oliveira <camilamoliveiratech@gmail.com>
2023-03-30 18:06:51 -0300

    Simplificando as funções ligadas ao botão de criptografar e descriptografar

--- script.js
+++ script.js
@@ -1,46 +1,33 @@
 var resultadoAnterior;          
-function traduzido(){
+function limparImagem(){
     document.getElementById("procurando").style.display ="none";        
 }
     
-function criptografar(){
+function botaoCriptografar(){
     const texto = document.getElementById("texto");
     if(!texto.checkValidity()){
-        document.getElementById("demo").innerHTML = texto.validationMessage;
-        document.getElementById("resultado").innerHTML = "";
+        mostrarErro();
+        limparElemento("resultado");
     } else{
-        document.getElementById("demo").innerHTML = "";
-        var mensagem = texto.value;
-
-        traduzido();
-        mensagem = mensagem.replaceAll("e", "enter");
-        mensagem = mensagem.replaceAll("i", "imes");
-        mensagem = mensagem.replaceAll("a","ai");
-        mensagem = mensagem.replaceAll("o", "ober");
-        mensagem = mensagem.replaceAll("u", "ufat");
-        mostraResultado(mensagem);
+        limparElemento("mensagemErro");
+        limparImagem();
+        mostrarResultado(criptografar(texto));
     }
 }
     
-function descriptografar(){
+function botaoDescriptografar(){
     const texto = document.getElementById("texto");
     if(!texto.checkValidity()){
-        document.getElementById("demo").innerHTML = texto.validationMessage;
-        document.getElementById("resultado").innerHTML = "";
+        mostrarErro();
+        limparElemento("resultado");
     } else{
-        document.getElementById("demo").innerHTML = "";
-        var mensagem = document.getElementById("texto").value;
-        traduzido();
-        mensagem = mensagem.replaceAll("enter", "e");
-        mensagem = mensagem.replaceAll("imes", "i");
-        mensagem = mensagem.replaceAll("ai","a");
-        mensagem = mensagem.replaceAll("ober", "o");
-        mensagem = mensagem.replaceAll("ufat", "u");
-        mostraResultado(mensagem);
+        limparElemento("mensagemErro");
+        limparImagem();
+        mostrarResultado(descriptografar(texto));
     }
 }
 
-function mostraResultado(mensagem){
+function mostrarResultado(mensagem){
     document.getElementById("resultado").innerHTML=mensagem;
 }
 
@@ -48,4 +35,32 @@ function copiarTexto(){
     let textocopiado = document.getElementById("resultado");
     textocopiado.select();
     document.execCommand("copy");
+}
+
+function mostrarErro(){
+    document.getElementById("demo").innerHTML = texto.validationMessage;
+}
+
+function limparElemento(id){
+    document.getElementById(id).innerHTML = "";
+}
+
+function criptografar(texto){
+    var mensagem = texto.value;
+    mensagem = mensagem.replaceAll("e", "enter");
+    mensagem = mensagem.replaceAll("i", "imes");
+    mensagem = mensagem.replaceAll("a","ai");
+    mensagem = mensagem.replaceAll("o", "ober");
+    mensagem = mensagem.replaceAll("u", "ufat");
+    return mensagem;
+}
+
+function descriptografar(texto){
+    var mensagem = texto.value;
+    mensagem = mensagem.replaceAll("enter", "e");
+    mensagem = mensagem.replaceAll("imes", "i");
+    mensagem = mensagem.replaceAll("ai","a");
+    mensagem = mensagem.replaceAll("ober", "o");
+    mensagem = mensagem.replaceAll("ufat", "u");
+    return mensagem;
 }

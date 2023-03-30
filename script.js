6cf49d35e847f4db2ebbfefcd89ba3d56e6bd33d
Camila Oliveira <camilamoliveiratech@gmail.com>
2023-03-30 17:32:01 -0300

    Complementei as funções de criptografar e descriptografar, para validar o texto apenas com letras minúsculas e sem acento

--- script.js
+++ script.js
@@ -4,25 +4,40 @@ function traduzido(){
 }
     
 function criptografar(){
-    var mensagem = document.getElementById("texto").value;
-    traduzido();
-    mensagem = mensagem.replaceAll("e", "enter");
-    mensagem = mensagem.replaceAll("i", "imes");
-    mensagem = mensagem.replaceAll("a","ai");
-    mensagem = mensagem.replaceAll("o", "ober");
-    mensagem = mensagem.replaceAll("u", "ufat");
-    mostraResultado(mensagem);
+    const texto = document.getElementById("texto");
+    if(!texto.checkValidity()){
+        document.getElementById("demo").innerHTML = texto.validationMessage;
+        document.getElementById("resultado").innerHTML = "";
+    } else{
+        document.getElementById("demo").innerHTML = "";
+        var mensagem = texto.value;
+
+        traduzido();
+        mensagem = mensagem.replaceAll("e", "enter");
+        mensagem = mensagem.replaceAll("i", "imes");
+        mensagem = mensagem.replaceAll("a","ai");
+        mensagem = mensagem.replaceAll("o", "ober");
+        mensagem = mensagem.replaceAll("u", "ufat");
+        mostraResultado(mensagem);
+    }
 }
     
 function descriptografar(){
-    var mensagem = document.getElementById("texto").value;
-    traduzido();
-    mensagem = mensagem.replaceAll("enter", "e");
-    mensagem = mensagem.replaceAll("imes", "i");
-    mensagem = mensagem.replaceAll("ai","a");
-    mensagem = mensagem.replaceAll("ober", "o");
-    mensagem = mensagem.replaceAll("ufat", "u");
-    mostraResultado(mensagem);
+    const texto = document.getElementById("texto");
+    if(!texto.checkValidity()){
+        document.getElementById("demo").innerHTML = texto.validationMessage;
+        document.getElementById("resultado").innerHTML = "";
+    } else{
+        document.getElementById("demo").innerHTML = "";
+        var mensagem = document.getElementById("texto").value;
+        traduzido();
+        mensagem = mensagem.replaceAll("enter", "e");
+        mensagem = mensagem.replaceAll("imes", "i");
+        mensagem = mensagem.replaceAll("ai","a");
+        mensagem = mensagem.replaceAll("ober", "o");
+        mensagem = mensagem.replaceAll("ufat", "u");
+        mostraResultado(mensagem);
+    }
 }
 
 function mostraResultado(mensagem){
